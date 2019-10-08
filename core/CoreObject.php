1d42a3d0cfdc6a589983ffe2363fc39ef9d9424f
Dade Brandon <dade.brandon@gmail.com>
2019-10-08 22:30:52 +0000

    Add explanation preferring magic properties over typed properties to the CoreObject

--- core/CoreObject.php
+++ core/CoreObject.php
@@ -21,6 +21,40 @@
 		other validation and parsing needs.
 
 		The getter/setter causes case sensitivity, but we're assuming lowerCamelCase for properties.....
+
+		----THEORY----
+		Other ORM frameworks use actual properties for their object/entity data.
+
+		The one advantage of this that I agree with, is that code completion will be able to assist with that
+		property via the code editor being able to read the class.
+
+		The downsides of this that cause me to be against that, are:
+			- Forces use of reflections for highly dynamic classes (for example, a parent
+				class that needs to know about its sibling class)
+			- Information can't be translated from an object of one type to another and back
+				with-out loss, for example a Person and User, where User is derived from Person,
+				and adds username/password, but Person doesn't contain these properties --
+				if we need Person and a User is serialized in to a Person due to being transmitted
+				or stored via JSON to a place expecting a Person, then User's properties won't
+				necessarily be stored or properly reloaded.
+			- PHP typehinting isn't (and probably will never be) as capable as HTML pattern input
+				checking; it does not support formatting, or the C equivalent of unions, or validation
+				of nested arrays, therefore indicating that a property must be an int, string, or otherwise,
+				isn't entirely useful when dealing with translating remote data (including user input).
+				For example, a phone number should be able to be entered in to an API that is expecting
+				a phone number in various formats, but then standardized when stored locally in to the
+				format most useful to us.    There is no PHP type for Phone Number that allows us to
+				type check that the property is being set to something that represents a valid phone number.
+
+			- Prefering for properties to by dynamically typed allows dynamically adding getters/setters
+				later with-out refactoring.  For example, Order->tax may inially be a property
+				with a value set when Order->subtotal is set (rather than dynamically calculated
+				via a getTax) - but then a reason comes up to use a getter
+				With this method, function getTax will be written, and Order->tax is still accesible,
+				whereas in the property paradigm, all code has to be re-written to access tax via getTax;
+				many developers resolve this by ALWAYS using getters and setters, even for properties that
+				aught not to need them, so that people know to just expect to call ->getTax() rather than
+				accessing ->tax -- this results in additional coding
 	*/
 
 	class CoreObject {
@@ -100,6 +134,16 @@
 			}
 
 			return $this->data[$key] = $value;
+		}
+
+		public function getDataArray()
+		{
+			$data = $this->data;
+
+			if (!isset($data["__class"]))
+				$data["__class"] = get_called_class();
+
+			return $data;
 		}
 
 	}
