dd3adc376f0f21f8d2f02c8a2dd9fe8c7733200a
Dade Brandon <dade.brandon@gmail.com>
2019-10-08 21:12:21 +0000

    Update the log message formatting to indent multi-line log properties to line up with the end of the property's key

--- core/Log/HandlerTrait.php
+++ core/Log/HandlerTrait.php
@@ -16,7 +16,7 @@
 			$column = 0;
 			foreach($eventData as $key => $value)
 			{
-				if (in_array($key, ["timestamp", "logType", "name"]))
+				if (in_array($key, ["timestamp", "logType", "name", "strings"]))
 					continue;
 
 				$varStr = str_pad(" ". $key .": ".var_export($value, TRUE), 30, " ");
@@ -25,12 +25,12 @@
 				{
 					if ($column % 2 != 0)
 					{
-						$str .= "\n";
+						$str .= "(a)\n";
 						$column++;
 					}
 				}
 
-				$newLinePad = ($column % 2 && strlen($varStr) <= 30) ? "" : str_pad("", 30, " ");
+				$newLinePad = str_pad("", (($column % 2 == 0) ? 0 : 30) + strlen($key)+2, " ");
 				$str .= str_replace("\n", "\n".$newLinePad, $varStr);
 
 				if ($column % 2 == 1 || strlen($varStr) > 30)
@@ -38,6 +38,7 @@
 					$str .= "\n";
 					$column = 0;
 				}
+				else $column++;
 			}
 
 			$str .= str_pad("", 60, "*")."\n\n";
